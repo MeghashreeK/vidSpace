aacf06894c94b7d146871a1eaba47fd1f557a7fb
MeghashreeK <kundermeghashree279@gmail.com>
2024-05-17 14:15:42 +0530

    youtube api setup

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+import { YOUTUBE_VIDEO_URL } from "../constants";
+
 const VideoContainer=()=>{
+    useEffect(()=>{
+        getData();
+    },[]);
+    
+    const getData=async()=>{
+        const data=await fetch(YOUTUBE_VIDEO_URL);
+        const json=await data.json();
+        console.log(json);
+    }
+
     return(
         <div>vdiocontainer</div>
     );
---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const YOUTUBE_API_KEY="AIzaSyCgbjye1AwOKvN6xeBXfO7IcQ6_GvOm79w"
+export const YOUTUBE_VIDEO_URL="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&key="+YOUTUBE_API_KEY;
