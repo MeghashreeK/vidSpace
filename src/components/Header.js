20a5d4da23d9bbba26c9b4505aa106d53feaf41a
MeghashreeK <kundermeghashree279@gmail.com>
2024-05-17 12:13:47 +0530

    sidebar functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,27 @@
+import { useDispatch } from "react-redux";
 import vidspacelogo from "../images/vidspace-logo.png";
-const Header=()=>{
-    return(
+import { toggleMenu } from "../utils/MenuSlice";
+const Header = () => {
+    const dispatch = useDispatch();
+    const toggleMenuBar = () => {
+        dispatch(toggleMenu());
+        // console.log(true);
+    }
+    return (
         <div className="grid grid-flow-col p-2 shadow-md shadow-slate-400">
             <div className="flex justify-center items-center col-span-1">
-           <img className="h-8 w-8" src="https://img.icons8.com/ios-filled/50/menu--v6.png" alt="menu--v6"/>
-           <img className="h-16 w-16" src={vidspacelogo} alt="logo"/>
-           </div>
-           <div className="flex col-span-10">
-            <p>Search</p>
-            <input type="text" className="border-black border" />
-           </div>
-           <div className="col-span-1">
-           <img className="h-8 w-8" width="24" height="24" src="https://img.icons8.com/material-rounded/24/000000/user-male-circle.png" alt="user-male-circle"/>
-           </div>
+                <img className="h-8 w-8" src="https://img.icons8.com/ios-filled/50/menu--v6.png" alt="menu--v6" onClick={toggleMenuBar} />
+                <img className="h-16 w-16" src={vidspacelogo} alt="logo" />
+            </div>
+            <div className="flex col-span-10">
+                <p>Search</p>
+                <input type="text" className="border-black border" />
+            </div>
+            <div className="col-span-1">
+                <img className="h-8 w-8" width="24" height="24" src="https://img.icons8.com/material-rounded/24/000000/user-male-circle.png" alt="user-male-circle" />
+            </div>
         </div>
-       
+
     );
 }
 export default Header;
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,6 +1,26 @@
-const SideBar=()=>{
-    return(
-        <div>SideBar</div>
-    );
+import { useSelector } from "react-redux";
+
+const SideBar = () => {
+    const isMenuOpen=useSelector((store)=>store.menu.isMenuOpen);
+    if(!isMenuOpen) return null;
+    return (
+        <ul>
+            <div className="flex">
+                <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/home--v2.png" alt="home--v2" />                <li>Home</li>
+            </div>
+            <div className="flex">
+                <img width="24" height="24" src="https://img.icons8.com/external-dashed-line-kawalan-studio/24/external-subscription-social-media-dashed-line-kawalan-studio.png" alt="external-subscription-social-media-dashed-line-kawalan-studio" />
+                <li>Subscriptions</li>
+            </div>
+            <div className="flex">
+                <img width="24" height="24" src="https://img.icons8.com/external-tal-revivo-regular-tal-revivo/24/external-like-thumbs-up-button-from-popular-social-media-logo-regular-tal-revivo.png" alt="external-like-thumbs-up-button-from-popular-social-media-logo-regular-tal-revivo" />            <li>Liked Videos</li>
+            </div>
+            <div className="flex">
+                <img width="24" height="24" src="https://img.icons8.com/external-tal-revivo-regular-tal-revivo/24/external-social-media-dislike-thumbs-down-gesture-in-square-logo-regular-tal-revivo.png" alt="external-social-media-dislike-thumbs-down-gesture-in-square-logo-regular-tal-revivo" />            <li>Disliked Videos</li>
+            </div>
+
+
+
+        </ul>);
 }
 export default SideBar;
