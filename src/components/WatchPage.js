5a1a0fd16810f331667beee40ba9dbc7595d34cf
MeghashreeK <kundermeghashree279@gmail.com>
2024-05-23 20:04:33 +0530

    added comment section

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,15 +1,28 @@
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/MenuSlice";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import { YOUTUBE_COMMENTS } from "../utils/constants";
 
 const WatchPage = () => {
     const dispatch = useDispatch();
     useEffect(() => {
     dispatch(closeMenu());
+    getComments();
     }, [])
     const [searchParams] = useSearchParams();
-    console.log(searchParams.get("v"))
+    console.log(searchParams.get("v"));
+
+    const [comments,setComments]=useState([]);
+
+const getComments=async()=>{
+    const data=await fetch(YOUTUBE_COMMENTS+searchParams.get("v"));
+    const json=await data.json();
+    // console.log(json.items[2]?.snippet?.topLevelComment?.snippet);
+    console.log(json)
+    setComments(json.items);
+}
+
 
     return (
         <div>
@@ -17,7 +30,24 @@ const WatchPage = () => {
         <iframe width="560" height="315" src={"https://www.youtube.com/embed/"+searchParams.get("v")} 
         title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
         referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-        <p>comments</p>
+        
+        <div className="flex flex-col border-2 border-black gap-1">
+        {comments.length>0 && comments.map((comment, index) => {
+                        const { authorDisplayName, authorProfileImageUrl, textOriginal } = comment?.snippet?.topLevelComment?.snippet || {};
+                        return (
+                            <div key={index} className="flex items-start gap-2">
+                                <img className="h-10 w-10 rounded-full" src={authorProfileImageUrl} alt="user-profile" />
+                                <div>
+                                    <p className="font-bold">{authorDisplayName}</p>
+                                    <p>{textOriginal}</p>
+                                </div>
+                            </div>
+                        );
+                    })}
+        </div>
+
+              
+
         </div>
         </div>
     );
