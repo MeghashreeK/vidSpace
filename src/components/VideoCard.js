63976a05e1606aad92a4a145dd779e205a9888b6
MeghashreeK <kundermeghashree279@gmail.com>
2024-05-26 12:31:02 +0530

    viewcount to M & K

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -3,19 +3,32 @@ const VideoCard = ({ videoInfo }) => {
     const { thumbnails, channelTitle, title } = snippet;
     const { viewCount } = statistics;
 
+    const formatViewCount = (count) => {
+        if (count >= 1000000) {
+            return (count / 1000000).toFixed(1) + 'M';
+        } else if (count >= 1000) {
+            return (count / 1000).toFixed(0) + 'K';
+        } else {
+            return count.toString();
+        }
+    };
+
+    const formattedViewCount = formatViewCount(viewCount);
+
+
     return (
         <div className="h-full">
         <div className="flex flex-col h-full">
             <img 
                 alt="thumbnail" 
                 src={thumbnails.medium.url}
-                className="w-full h-48 object-cover"
+                className="w-full h-48 object-cover rounded-lg"
             />
             <div className="p-4 flex flex-col justify-between h-full">
                 <ul>
-                    <li className="font-bold break-words">{title}</li>
+                    <li className="font-bold truncate-2-lines">{title}</li>
                     <li className="text-slate-400">{channelTitle}</li>
-                    <li className="text-slate-400">{viewCount}views</li>
+                    <li className="text-slate-400">{formattedViewCount} views</li>
                 </ul>
             </div>  
         </div>
