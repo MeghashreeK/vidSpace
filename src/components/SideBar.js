24b8d83544da9eb8251d5c885ef12b3840dc6910
MeghashreeK <kundermeghashree279@gmail.com>
2024-05-26 22:11:12 +0530

    views using slice

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -4,23 +4,22 @@ const SideBar = () => {
     const isMenuOpen=useSelector((store)=>store.menu.isMenuOpen);
     if(!isMenuOpen) return null;
     return (
-        <div className="bg-black border-2 w-2/6">
-        <ul className="text-white">
-            <div className="flex">
-                <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/home--v2.png" alt="home--v2" />                
-                <Link to="/"><li>Home</li></Link>
+        <div className="bg-black w-2/6 flex justify-center">
+        <ul className="text-white flex flex-col gap-3 w-full px-2">
+            <div className="flex items-center gap-1 hover:bg-[#1f1f1f] hover:rounded-lg p-1">
+            <img className="w-5 h-4" src="https://img.icons8.com/material-sharp/24/ffffff/home.png" alt="home"/>
+            <Link to="/"><li>Home</li></Link>
             </div>
-            <div className="flex">
-                <img width="24" height="24" src="https://img.icons8.com/external-dashed-line-kawalan-studio/24/external-subscription-social-media-dashed-line-kawalan-studio.png" alt="external-subscription-social-media-dashed-line-kawalan-studio" />
-                <li>Subscriptions</li>
+            <div className="flex items-center gap-1 hover:bg-[#1f1f1f] hover:rounded-lg p-1">
+            <img className="w-5 h-4" src="https://img.icons8.com/material-sharp/24/ffffff/musical-notes.png" alt="musical-notes"/>
+            <li>Music</li>
             </div>
-            <div className="flex">
-                <img width="24" height="24" src="https://img.icons8.com/external-tal-revivo-regular-tal-revivo/24/external-like-thumbs-up-button-from-popular-social-media-logo-regular-tal-revivo.png" alt="external-like-thumbs-up-button-from-popular-social-media-logo-regular-tal-revivo" />            
-                <Link to="/live"><li>Live Videos</li></Link>
+            <div className="flex items-center gap-1 hover:bg-[#1f1f1f] hover:rounded-lg p-1">
+            <img className="w-5 h-4" src="https://img.icons8.com/ios-glyphs/30/ffffff/cinema---v2.png" alt="cinema---v2"/>                <li>Movies</li>
             </div>
-            <div className="flex">
-                <img width="24" height="24" src="https://img.icons8.com/external-tal-revivo-regular-tal-revivo/24/external-social-media-dislike-thumbs-down-gesture-in-square-logo-regular-tal-revivo.png" alt="external-social-media-dislike-thumbs-down-gesture-in-square-logo-regular-tal-revivo" />            
-                <li>Disliked Videos</li>
+            <div className="flex items-center gap-1 hover:bg-[#1f1f1f] hover:rounded-lg p-1">
+            <img className="w-5 h-4" src="https://img.icons8.com/windows/32/ffffff/youtube-live.png" alt="youtube-live"/>
+            <Link to="/live"><li>Live Videos</li></Link>
             </div>
         </ul>
         </div>);
--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -5,7 +5,8 @@ const VideoCard = ({ videoInfo }) => {
 
     const formatViewCount = (count) => {
         if (count >= 1000000) {
-            return (count / 1000000).toFixed(1) + 'M';
+            const millionCount = (count / 1000000).toFixed(1);
+            return millionCount.endsWith('.0') ? millionCount.slice(0, -2) + 'M' : millionCount + 'M';
         } else if (count >= 1000) {
             return (count / 1000).toFixed(0) + 'K';
         } else {
@@ -17,22 +18,23 @@ const VideoCard = ({ videoInfo }) => {
 
     return (
         <div className="h-full">
-        <div className="flex flex-col h-full">
-            <img 
-                alt="thumbnail" 
-                src={thumbnails.standard.url}
-                className="w-full h-48 object-cover rounded-lg"
-            />
-            <div className="p-4 flex flex-col justify-between h-full">
-                <ul>
-                    <li className="font-bold truncate-2-lines text-white">{title}</li>
-                    <li className="text-slate-400">{channelTitle}</li>
-                    <li className="text-slate-400">{formattedViewCount} views</li>
-                </ul>
-            </div>  
+            <div className="flex flex-col h-full">
+                <img
+                    alt="thumbnail"
+                    src={thumbnails.standard.url}
+                    className="w-full h-48 object-cover rounded-lg"
+                />
+                <div className="p-4 flex flex-col justify-between h-full">
+                    <ul>
+                        <li className="font-bold truncate-2-lines text-white">{title}</li>
+                        <li className="text-slate-400">{channelTitle}</li>
+                        <li className="text-slate-400">{formattedViewCount} views</li>
+                    </ul>
+                </div>
+            </div>
         </div>
-    </div>
     );
 }
 export default VideoCard;
+
 
