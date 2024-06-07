f2ae689e155d6fd6f1d0394e67aef6b464378647
MeghashreeK <kundermeghashree279@gmail.com>
2024-06-07 14:44:49 +0530

    live page css

--- src/components/MainVideoCard.js
+++ src/components/MainVideoCard.js
@@ -1,17 +1,35 @@
 import React from 'react'
 
 const MainVideoCard = ({ videoInfo }) => {
-    const { snippet} = videoInfo;
+    const { snippet } = videoInfo;
     const { thumbnails, title, channelTitle } = snippet;
 
     return (
-        <div className="h-full rounded-lg bg-[#1f1f1f]">
+        // <div className="h-full rounded-lg bg-[#1f1f1f]">
+        //     <div className="flex flex-col h-full">
+        //         <img
+        //             alt="thumbnail"
+        //             src={thumbnails.high.url}
+        //             className="w-full sm:h-48 h-[10rem] object-cover rounded-lg"
+        //         />
+        //         <div className="p-4 flex flex-col justify-between h-full">
+        //             <ul>
+        //                 <li className="font-bold truncate-2-lines text-white">{title}</li>
+        //                 <li className="text-slate-400">{channelTitle}</li>
+        //             </ul>
+        //         </div>
+        //     </div>
+        // </div>
+
+        <div className="h-full rounded-lg bg-[#1f1f1f] border-2 border-red-700">
             <div className="flex flex-col h-full">
-                <img
-                    alt="thumbnail"
-                    src={thumbnails.high.url}
-                    className="w-full h-48 object-cover rounded-lg"
-                />
+                <div className="relative overflow-hidden rounded-lg" style={{ paddingTop: '56.25%' }}>
+                    <img
+                        alt="thumbnail"
+                        src={thumbnails.high.url}
+                        className="absolute top-0 left-0 w-full h-full object-cover"
+                    />
+                </div>
                 <div className="p-4 flex flex-col justify-between h-full">
                     <ul>
                         <li className="font-bold truncate-2-lines text-white">{title}</li>
